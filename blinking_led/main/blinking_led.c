95d5a8042a9b0ab7089bd4630f53ef1174bb4858
b3nj4m1n <b3nj4m1n@gmx.net>
2022-06-02 20:00:15 +0200

    Formatting

--- blinking_led/main/blinking_led.c
+++ blinking_led/main/blinking_led.c
@@ -15,16 +15,16 @@ int millisecond(int millisecond) { return (millisecond / portTICK_PERIOD_MS); }
 void sleep_seconds(int seconds) { vTaskDelay(millisecond(seconds * 1000)); }
 
 void app_main(void) {
-  char *task_name = pcTaskGetName(NULL);
-  ESP_LOGI(task_name, "Light it up\n");
+    char *task_name = pcTaskGetName(NULL);
+    ESP_LOGI(task_name, "Light it up\n");
 
-  gpio_reset_pin(LED_PIN);
-  gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
-  while (1) {
-    gpio_set_level(LED_PIN, 1);
-    sleep_seconds(1);
-    gpio_set_level(LED_PIN, 0);
-    sleep_seconds(1);
-  }
+    while (1) {
+        gpio_set_level(LED_PIN, 1);
+        sleep_seconds(1);
+        gpio_set_level(LED_PIN, 0);
+        sleep_seconds(1);
+    }
 }
